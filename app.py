19283abc30a6d87868813b117aa2775d1d947402
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-05-17 21:35:10 +0200

    feat(influx): add influx output option

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
+import math
 import os
 import sys
-import matplotlib.pyplot as plt
-import pandas as pd
-from darts.timeseries import TimeSeries
-from darts.models.forecasting.fft import FFT
-from darts.models.forecasting.nbeats import NBEATSModel
-from darts.utils.missing_values import fill_missing_values
+
 from darts.metrics import mae
 from darts.models.filtering.moving_average_filter import MovingAverageFilter
-import math
+from darts.models.forecasting.fft import FFT
+from darts.models.forecasting.nbeats import NBEATSModel
+from darts.timeseries import TimeSeries
+import pandas as pd
 
 # parse environment variables
 prediction_model = os.environ.get('PREDICTION_MODEL', 'fft')
 prediction_model_epochs = os.environ.get('PREDICTION_MODEL_EPOCHS', 30)
 prediction_split = os.environ.get('PREDICTION_SPLIT')
-prediction_metric_name = os.environ.get('PREDICTION_METRIC_NAME', 'prediction')
 prediction_count = os.environ.get('PREDICTION_COUNT')
 
 # parse file info
@@ -25,6 +23,7 @@ input_valuecol = os.environ.get('INPUT_VALUECOL', 'value')
 input_movingaverage = int(os.environ.get('INPUT_MOVINGAVERAGE', False))
 
 output_filename = os.environ.get('OUTPUT_FILENAME', '/volume/prediction.csv')
+output_format = os.environ.get('OUTPUT_FORMAT', 'csv')
 
 print('reading from file:', input_filename)
 print('timecol          :', input_timecol)
@@ -47,31 +46,31 @@ if 'name' in df.columns:
     measurement_name = df['name'].unique()[0] 
     output_measurement_name = measurement_name + '-prediction'
 
+
+
 if input_movingaverage: 
     original = series
     ma = MovingAverageFilter(window=input_movingaverage)
     y_filtered = ma.filter(series)
-    y_filtered.plot(label="filter")
     series = y_filtered
-
 
 #series = fill_missing_values(series)
 
 ##TODO: extract to different file / functions
 if prediction_model == 'nbeats':
     model = NBEATSModel(
-        input_chunk_length=30,
-        output_chunk_length=30,
-        generic_architecture=True,
-        num_stacks=10,
-        num_blocks=1,
-        num_layers=4,
-        layer_widths=512,
-        n_epochs=int(prediction_model_epochs),
-        nr_epochs_val_period=1,
-        batch_size=800,
-        model_name="nbeats_run",
-    )
+            input_chunk_length=30,
+            output_chunk_length=30,
+            generic_architecture=True,
+            num_stacks=10,
+            num_blocks=1,
+            num_layers=4,
+            layer_widths=512,
+            n_epochs=int(prediction_model_epochs),
+            nr_epochs_val_period=1,
+            batch_size=800,
+            model_name="nbeats_run",
+            )
     if prediction_split:
         train, val = series.split_before(float(prediction_split))
     else:
@@ -98,9 +97,19 @@ else:
     else:
         pred_val = model.predict(len(val)*2)
 
-
 print("MAE:", mae(pred_val, val))
-pred_val.to_csv(path_or_buf=output_filename, date_format='%s%f000')
+if output_format == 'influx':
+    lines = []
+    for i in range(len(pred_val)):
+        timestamp = pred_val.time_index[i].value
+        value = pred_val.values()[i][0]
+        line = f"{output_measurement_name} value={value} {timestamp}"
+        lines.append(line)
+    with open(output_filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+else:
+    pred_val.to_csv(path_or_buf=output_filename, date_format='%s%f000')
 
 #train.plot(label="train")
 #val.plot(label="val")
