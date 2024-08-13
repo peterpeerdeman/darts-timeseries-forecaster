0f3e9554e054828b79a5869d9f2caeeb477689f9
Peter Peerdeman <peterpeerdeman@gmail.com>
2024-08-13 14:12:48 +0200

    fix(build): get build running with new dependencies without nbeats

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='darts-timeseries-forecaster',
-    version='1.0.7',
+    version='1.0.8',
     author='Peter Peerdeman',
     packages=find_packages(),
-    install_requires=[
-        'darts==0.29.0'
-    ],
 )
